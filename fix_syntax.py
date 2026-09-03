05ada20cc6256bcadae279aaefe635c911614262
omarHaraz <omarharaz553@gmail.com>
2026-09-03 11:40:36 +0300

    chore: purge extraneous files, isolate frontend and backend

--- fix_syntax.py
+++
@@ -1,9 +0,0 @@
-js_path = r"C:\Users\Logo\Desktop\Psychometric\frontend\candidate\assets\js\candidate-app.js"
-with open(js_path, "r", encoding="utf-8") as f:
-    js = f.read()
-
-js = js.replace("showView(\\\"view-pending-portal\\\");", "showView(\"view-pending-portal\");")
-js = js.replace("showView(\\\"view-instructions\\\");", "showView(\"view-instructions\");")
-
-with open(js_path, "w", encoding="utf-8") as f:
-    f.write(js)
